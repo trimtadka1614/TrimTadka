cc21c0e3be4b9ec3ac614152d9697dc135228935
Sourjya_1614 <159236410+Sourjya-Saha@users.noreply.github.com>
2025-07-29 17:08:50 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -2753,9 +2753,6 @@ app.put('/employees/:emp_id/status', async (req, res) => {
         res.status(500).json({ error: 'Server error while updating employee status.' });
     }
 });
-
-
-
 // Start the server
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
