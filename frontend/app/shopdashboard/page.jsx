686538a2cdbbc672d767e48b4b04d3e718c8a57b
Sourjya_1614 <159236410+Sourjya-Saha@users.noreply.github.com>
2025-07-29 17:10:01 +0530

    Update page.jsx

--- frontend/app/shopdashboard/page.jsx
+++ frontend/app/shopdashboard/page.jsx
@@ -3,7 +3,6 @@ import { useSession, signOut } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 import { useEffect, useState, useCallback } from 'react';
 import axios from 'axios';
-
 import dayjs from 'dayjs'; // For date formatting
 import {
     UsersIcon,
