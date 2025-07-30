ed2fab7954381eaadafb9cdfcf5a66f16f76a5fb
trimtadka1614 <trimtadka@gmail.com>
2025-07-31 02:41:16 +0530

    Update and rename tailwind.config.js to tailwind.config.mjs

--- frontend/tailwind.config.mjs
+++ frontend/tailwind.config.mjs
@@ -1,13 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
     './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
 }
