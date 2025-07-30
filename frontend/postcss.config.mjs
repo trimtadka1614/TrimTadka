7817b19b56c31c0b6ea84546136b307419761c19
trimtadka1614 <trimtadka@gmail.com>
2025-07-31 02:24:19 +0530

    Update postcss.config.mjs

--- frontend/postcss.config.mjs
+++ frontend/postcss.config.mjs
@@ -1,5 +1,11 @@
 const config = {
-  plugins: ["@tailwindcss/postcss"],
+  plugins: {
+    'postcss-custom-properties': {
+      preserve: false,
+    },
+    '@tailwindcss/postcss': {},
+    'autoprefixer': {},
+  },
 };
 
 export default config;
