04d284dc3d7edbf1e106c08797462f8123bba541
trimtadka1614 <trimtadka@gmail.com>
2025-07-31 21:36:11 +0530

    Update page.jsx

--- frontend/app/userdashboard/page.jsx
+++ frontend/app/userdashboard/page.jsx
@@ -1341,7 +1341,7 @@ function urlB64ToUint8Array(base64String) {
           }
         `}</style>
 
-        <header className="header-blur shadow-lg border-b border-white/20 sticky top-0 z-40">
+        <header className="backdrop-blur shadow-lg border-b border-white/20 sticky top-0 z-40">
   <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-[5px]">
     <div className="flex items-center justify-between">
       {/* Logo */}
